de53141c72f0613decfd19f95ee3142401930715
Nitchakan <nitchakan.kkng@gmail.com>
2024-11-22 13:55:26 +0700

    fix web cam

--- 241121-005744-esp32cam/src/CameraWebServer.ino
+++ 241121-005744-esp32cam/src/CameraWebServer.ino
@@ -36,8 +36,10 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "PP";
-const char *password = "ppaaoo48";
+// const char *ssid = "PP";
+// const char *password = "ppaaoo48";
+const char *ssid = "S22";
+const char *password = "nawakorn";
 
 void startCameraServer();
 void setupLedFlash(int pin);
